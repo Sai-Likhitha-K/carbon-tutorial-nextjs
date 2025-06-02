81440f3667efebd7b22d784e1cda652c0a3941f1
Sai-Likhitha-K <iamksai2001@gmail.com>
2025-06-02 23:02:56 +0530

    feat(tutorial): complete step 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+'use client';
+import { Grid, Column } from '@carbon/react';
 import LandingPage from './home/page';
 
 export default function Page() {
-    return <LandingPage />;
-  }
+  return <LandingPage />;
+}
